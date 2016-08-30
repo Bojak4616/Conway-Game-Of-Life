2072a78db89ae7882b123d37da38ea7bfd43a148
Jesse Buonanno <jpb4616@rit.edu>
2016-08-29 21:04:13 -0400

    basic game working without user interaction

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from time import sleep
+from os import system
 
 class Conway:
 	def __init__ (self, width, height):
@@ -20,22 +21,28 @@ class Conway:
 		for cell_height in xrange(self.height):
 			for cell_width in xrange(self.width):
 				board += self.map[cell_height][cell_width]
-
 			board += '\n'
 		print board
 
 	def neighbors_alive(self, x, y):
 		live_cells = 0
-
-		++live_cells if self.self_alive(x+1,y-1)
-		++live_cells if self.self_alive(x-1,y+1)
-		++live_cells if self.self_alive(x+1,y+1)
-		++live_cells if self.self_alive(x,y+1)
-		++live_cells if self.self_alive(x+1,y)
-		++live_cells if self.self_alive(x-1,y-1)
-		++live_cells if self.self_alive(x-1,y)
-		++live_cells if self.self_alive(x,y-1)
-
+	 
+		if self.self_alive(x+1,y-1):
+			live_cells += 1
+		if self.self_alive(x-1,y+1):
+			live_cells += 1
+		if self.self_alive(x+1,y+1):
+			live_cells += 1
+		if self.self_alive(x,y+1):
+			live_cells += 1
+		if self.self_alive(x+1,y):
+			live_cells += 1
+		if self.self_alive(x-1,y-1):
+			live_cells += 1
+		if self.self_alive(x-1,y):
+			live_cells += 1
+		if self.self_alive(x,y-1):
+			live_cells += 1
 
 		return live_cells
 
@@ -48,10 +55,47 @@ class Conway:
 	def set_cell(self, x, y):
 		self.map[x][y] = '+'
 
+	def game_tick(self):
+		tmp_map = []
+		for y in xrange(self.height):
+			tmp_map.append([])
+			for x in xrange(self.width):
+				tmp_map[y].append('-')
+
+		for y in xrange(self.height):
+			for x in xrange(self.width):		
+				#Rules
+				if self.neighbors_alive(x, y) < 2:
+					tmp_map[x][y] = '-'
+
+				if self.self_alive(x, y) == True and (self.neighbors_alive(x, y) == 2 or self.neighbors_alive(x, y) == 3):
+					tmp_map[x][y] = '+'
+
+				if self.self_alive(x, y) == True and self.neighbors_alive(x, y) > 3:
+					tmp_map[x][y] = '-'
+
+				if self.self_alive(x, y) == False and self.neighbors_alive(x, y) == 3:
+					tmp_map[x][y] = '+'
+
+		self.map = tmp_map
 
 if __name__ == '__main__':
 
+	print(chr(27) + "[2J")
 	NewGame = Conway(20,20)
 	NewGame.generate_world()
-	NewGame.print_game_board()
+	NewGame.set_cell(2, 1)
+	NewGame.set_cell(3, 2)
+	NewGame.set_cell(1, 3)
+	NewGame.set_cell(2, 3)
+	NewGame.set_cell(3, 3)
+
+
+	while(True):
+		sleep(.3)
+		print(chr(27) + "[2J")
+		NewGame.print_game_board()
+		NewGame.game_tick()
+
+
 
